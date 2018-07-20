f73c00a94670bc2d4b19b923c88337629d590e64
smibbie <35998407+smibbie@users.noreply.github.com>
2018-07-20 16:05:05 -0400

    spotify functionality added, working on twitter

--- liri.js
+++ liri.js
@@ -1,21 +1,45 @@
 require("dotenv").config();
 
+// GET KEYS.JS & ANY ADDITIONAL REQUIRED FILES & PACKAGES -
 let keys = require("./keys.js");
 let request = require("request");
+let Twitter = require('twitter');
+let Spotify = require('node-spotify-api')
+
+// SPOTIFY API -------------------------------------------
 let spotify = new Spotify(keys.spotify);
 let client = new Twitter(keys.twitter);
 
+// GRABBING COMMAND LINE VALUES --------------------------
 let command = process.argv[2];
 let media = process.argv[3];
 
+// TWITTER COMMANDS ---------------------------------------
 if (command === 'my-tweets') {
-
+  console.log(client);
+  let params = {screen_name: 'britney76944406'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      console.log(tweets);
+    }
+  });
 }
 
+// SPOTIFY COMMANDS --------------------------------------
 if (command === 'spotify-this-song') {
-  
+  spotify.search({ type: 'track', query: `${media}`, limit: 1 }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+
+  console.log("The name of the song is: " + data.tracks.items[0].name);
+  console.log("The artist's name is: " + data.tracks.items[0].album.artists[0].name);
+  console.log("The album name of this song is: " + data.tracks.items[0].album.name);
+  console.log("The URL to listen to this song: " + data.tracks.items[0].album.external_urls.spotify);
+  });
 }
 
+// MOVIE COMMANDS ----------------------------------------
 if (command === 'movie-this') {
   request(`http://www.omdbapi.com/?t=${media}&y=&plot=short&apikey=trilogy`, function(error, response, content) {
     if (!error && response.statusCode === 200) {
@@ -33,6 +57,7 @@ if (command === 'movie-this') {
   });
 }
 
+// RANDOM COMMANDS ------------------------------------------
 if (command === 'do-what-it-says') {
 
 }
